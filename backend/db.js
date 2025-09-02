4849b05ef4ba54bc15018a49bbca4056edb105dd
awais7796 <awaitzz49@gmail.com>
2025-09-02 12:12:38 +0530

    connect db and middlewares

---
+++ backend/db.js
@@ -0,0 +1,42 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb+srv://awaitzz49:Demo123@cluster0.blhgy.mongodb.net/paytm_clone");
+
+
+const userSchema= new mongoose.Schema({
+    username:{
+        type:true,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        minLenght:6,
+        maxLenght:8
+
+    },
+    password:{
+        type:true,
+        required:true,
+        unique:true,
+        minLenght:6
+
+    },
+    firstName:{
+        type:true,
+        required:true,
+        trim:true,
+        minLenght:6
+
+
+    },
+    lastName:{
+        type:true,
+        required:true,
+        trim:true,
+        minLenght:6
+    }
+})
+const User=mongoose.model('User',userSchema);
+module.exports={
+    User}
+    ;
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,23 @@
 const express = require("express");
+const cors=require("cors")
+const app=express();
+const   PORT=3000
 
+app.use(cors());
+app.use(express.json());
+
+
+//importing router 
+const rootRouter=require("./router/index")
+const userRouter=require("./router/user.router")
+
+//routing 
+app.use("/api/v1",rootRouter);
+app.use("/api/v1/user",userRouter);
+
+
+app.listen(PORT,(req,res)=>{
+    console.log(`server is runninh on post ${PORT}`);
+    
+})
 
