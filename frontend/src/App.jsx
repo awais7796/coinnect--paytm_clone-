92f352e8cc17080978c441e8cc60e72cf5fa6412
awais7796 <awaitzz49@gmail.com>
2025-10-01 15:56:55 +0530

    added dashboard and signin route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup.jsx";
+import Signin from "./pages/Signin.jsx";
+import { SendMoney } from "./pages/SendMoney.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 import "./index.css";
 
 function App() {
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
       </Routes>
     </BrowserRouter>
   );
