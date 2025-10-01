2e693a67f92885e42ba4c4c36326acbdaf4e9c20
awais7796 <awaitzz49@gmail.com>
2025-10-01 15:52:50 +0530

    add and style Signin page

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,5 +1,57 @@
+import Heading from "../components/Heading";
+import Subheading from "../components/Subheading";
+import Button from "../components/Button";
+import InputBox from "../components/InputBox";
+import BottomWarning from "../components/BottomWarning";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const Signin = () => {
-  return <div className="">Signin</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="bg-slate-300 h-screen flex justify-center">
+      <div className="flex justify-center flex-col">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+          <Heading label={"Sign in"} />
+          <Subheading
+            label={"Enter Your credentials to access your account "}
+          />
+          <InputBox
+            label={"Email"}
+            placeholder={"Enter your email"}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <InputBox
+            label={"Password"}
+            placeholder={" "}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <div className="pt-4">
+            <Button
+              label={"Sign in "}
+              onClick={async (e) => {
+                const response = await axios.post(
+                  "http://localhost:3000/api/v1/user/signin"
+                );
+                useNavigate("/dashboard");
+              }}
+            />
+            <BottomWarning
+              label={"Dont have an account ?"}
+              to={"/signup"}
+              buttontext={"Sign up"}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
